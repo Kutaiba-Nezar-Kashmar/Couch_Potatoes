3781f44594f3d649cf7d8ab07e86df88d5ecdc64
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2023-04-14 15:02:16 +0200

    cast and crew domain (michael kutaiba kasper)

---
+++ src/Services/MovieInformation/MovieInformation.Domain/Models/Cast.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieInformation.Domain.Models;
+
+public class Cast : MoviePerson
+{
+    public int CastId { get; set; }
+    public string Character { get; set; }
+    public int Order { get; set; }
+}
---
+++ src/Services/MovieInformation/MovieInformation.Domain/Models/Crew.cs
@@ -0,0 +1,7 @@
+﻿namespace MovieInformation.Domain.Models;
+
+public class Crew : MoviePerson
+{
+    public string Department { get; set; }
+    public string Job { get; set; }
+}
