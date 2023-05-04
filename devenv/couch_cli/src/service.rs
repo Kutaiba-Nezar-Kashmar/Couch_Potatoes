46b7e651cd1b2d900e888573902a11acacf45dc4
Michael Bui <michaelbui99@hotmail.com>
2023-05-04 22:17:12 +0200

    Expanded cli tool to automatically add project references and install dependencies

--- devenv/couch_cli/src/service.rs
+++ devenv/couch_cli/src/service.rs
@@ -14,60 +14,38 @@ pub fn service_cmd() -> Command {
 }
 
 pub fn create_new_service(name: &String) {
-    let service_path = Path::new("./src/Services/");
-    println!("Creating new service: {name}");
-    cd(Path::new(&get_project_root_dir()));
-    cd(service_path);
-    fs::create_dir(format!("./{name}")).expect("Failed to create dir");
-    cd(Path::new(format!("./{name}").as_str()));
+    println!("Service to create: {name}");
+    cd(&get_project_root_dir());
+    cd("./src/Services/");
+    mkdir(format!("./{name}").as_str());
+    cd(format!("./{name}").as_str());
+
+    let API_PROJECT_NAME = format!("{name}.API");
+    let APPLICATION_PROJECT_NAME = format!("{name}.Application");
+    let DOMAIN_PROJECT_NAME = format!("{name}.Domain");
+    let INFRASTRUCTURE_PROJECT_NAME = format!("{name}.Infrastructure");
 
     println!("Creating new solution...");
     let mut output = create_solution(name);
     let solution_file = format!("{name}.sln");
 
     println!("Creating new API project...");
-    output = create_dotnet_project("webapi", format!("{name}.API").as_str());
+    output = create_dotnet_project("webapi", &API_PROJECT_NAME);
     print_output(output);
 
     println!("Creating new Application project...");
-    output = create_dotnet_project("classlib", format!("{name}.Application").as_str());
+    output = create_dotnet_project("classlib", &APPLICATION_PROJECT_NAME);
     print_output(output);
 
     println!("Creating new Domain project...");
-    output = create_dotnet_project("classlib", format!("{name}.Domain").as_str());
+    output = create_dotnet_project("classlib", &DOMAIN_PROJECT_NAME);
     print_output(output);
 
     println!("Creating new Infrastricture project...");
-    output = create_dotnet_project("console", format!("{name}.Infrastructure").as_str());
+    output = create_dotnet_project("console", &INFRASTRUCTURE_PROJECT_NAME);
     print_output(output);
 
-    let paths = fs::read_dir("./").unwrap().into_iter();
-
-    for path in paths {
-        match path {
-            Ok(dir_entry) => {
-                let isDir = match dir_entry.file_type().as_ref() {
-                    Ok(file) => file.is_dir(),
-                    Err(_) => false,
-                };
-
-                let file_name = String::from(
-                    dir_entry
-                        .file_name()
-                        .to_str()
-                        .expect("Failed to read file name"),
-                );
-                if isDir && file_name.contains(name) {
-                    let csproj_path = format!("./{file_name}/{file_name}.csproj");
-                    println!("{csproj_path}");
-                    println!("Adding {csproj_path} to solution...");
-                    output = add_project_to_solution(&solution_file, &csproj_path);
-                    print_output(output);
-                }
-            }
-            Err(_) => exit(1),
-        }
-    }
+    visit_service_project_folders_in_current_dir(name, post_service_project_creation);
 }
 
 fn get_project_root_dir() -> String {
@@ -88,7 +66,7 @@ fn get_project_root_dir() -> String {
     if path_parts.into_iter().any(|p| p == project_name) {
         let mut at_project_root = false;
         while !at_project_root {
-            cd(Path::new(".."));
+            cd("..");
             current_dir = get_current_dir();
             let path_parts = current_dir.split("\\");
             let path_parts = path_parts.collect::<Vec<&str>>();
@@ -109,8 +87,12 @@ fn get_current_dir() -> String {
     String::from(env::current_dir().unwrap().as_os_str().to_str().unwrap())
 }
 
-fn cd(path: &Path) {
-    env::set_current_dir(path).expect("Failed to change dir");
+fn cd(path: &str) {
+    env::set_current_dir(Path::new(path)).expect("Failed to change dir");
+}
+
+fn mkdir(name: &str) {
+    fs::create_dir(Path::new(name)).expect("Failed to create new dir");
 }
 
 fn create_solution(name: &str) -> Output {
@@ -120,14 +102,14 @@ fn create_solution(name: &str) -> Output {
         .expect(format!("Failed to create new solution {name}").as_str())
 }
 
-fn create_dotnet_project(template: &str, name: &str) -> Output {
+fn create_dotnet_project(template: &str, name: &String) -> Output {
     process::Command::new("dotnet")
         .args(["new", template, "-o", format!("{name}").as_str()])
         .output()
         .expect(format!("Failed to create new Web API for service {name}").as_str())
 }
 
-fn add_project_to_solution(sln_file: &str, csproj_path: &str) -> Output {
+fn add_project_to_solution(sln_file: &String, csproj_path: &String) -> Output {
     process::Command::new("dotnet")
         .args([
             "sln",
@@ -142,4 +124,99 @@ fn add_project_to_solution(sln_file: &str, csproj_path: &str) -> Output {
 fn print_output(output: Output) {
     println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
     println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+}
+
+/// on_visit takes a lambda with parameters: DirEntry and &str, where 1st &str is service_name and 2nd &str is the project folder name
+fn visit_service_project_folders_in_current_dir(service_name: &String, on_visit: fn(&String, DirEntry, &String)) {
+    let paths = fs::read_dir("./").unwrap().into_iter();
+
+    for path in paths {
+        match path {
+            Ok(dir_entry) => {
+                let isDir = match dir_entry.file_type().as_ref() {
+                    Ok(file) => file.is_dir(),
+                    Err(_) => false,
+                };
+
+                let file_name = String::from(
+                    dir_entry
+                        .file_name()
+                        .to_str()
+                        .expect("Failed to read file name"),
+                );
+
+                if isDir && file_name.contains(service_name) {
+                    on_visit(service_name, dir_entry, &file_name);
+                }
+            }
+            Err(_) => exit(1),
+        }
+    }
+}
+
+fn add_project_reference(source_project_csproj_path: &str, dependency_project_csproj_path: &str) -> Output {
+    process::Command::new("dotnet")
+        .args(["add", source_project_csproj_path, "reference", dependency_project_csproj_path])
+        .output()
+        .expect("Failed to add reference")
+}
+
+fn add_nuget_package_to_project(csproj_path: &str, package_name: &str) -> Output {
+    process::Command::new("dotnet")
+        .args(["add", csproj_path, "package", package_name])
+        .output()
+        .expect("Failed to add dependency")
+}
+
+fn post_service_project_creation(service_name: &String, entry_dir: fs::DirEntry, file_name: &String) {
+    let API_PROJECT_NAME = format!("{service_name}.API");
+    let APPLICATION_PROJECT_NAME = format!("{service_name}.Application");
+    let DOMAIN_PROJECT_NAME = format!("{service_name}.Domain");
+    let INFRASTRUCTURE_PROJECT_NAME = format!("{service_name}.Infrastructure");
+
+    let solution_file = format!("{service_name}.sln");
+    let api_csproj = format!("{API_PROJECT_NAME}/{API_PROJECT_NAME}.csproj");
+    let application_csproj = format!("{APPLICATION_PROJECT_NAME}/{APPLICATION_PROJECT_NAME}.csproj");
+    let domain_csproj = format!("{DOMAIN_PROJECT_NAME}/{DOMAIN_PROJECT_NAME}.csproj");
+    let infrastructure_csproj = format!("{INFRASTRUCTURE_PROJECT_NAME}/{INFRASTRUCTURE_PROJECT_NAME}.csproj");
+
+    let csproj_path = format!("./{file_name}/{file_name}.csproj");
+    println!("Adding {csproj_path} to solution...");
+    let mut output = add_project_to_solution(&solution_file, &csproj_path);
+    print_output(output);
+
+    match file_name.to_string() {
+        project if project.contains(".API") => {
+            println!("Setting up references and installing packages for: {file_name}");
+            output = add_project_reference(csproj_path.as_str(), &application_csproj);
+            print_output(output);
+
+            output = add_project_reference(csproj_path.as_str(), &domain_csproj);
+            print_output(output);
+
+            output = add_project_reference(csproj_path.as_str(), &infrastructure_csproj);
+            print_output(output);
+
+            output = add_nuget_package_to_project(csproj_path.as_str(), "MediatR");
+            print_output(output);
+        }
+        project if project.contains(".Application") => {
+            output = add_project_reference(csproj_path.as_str(), &domain_csproj);
+            print_output(output);
+
+            output = add_project_reference(csproj_path.as_str(), &infrastructure_csproj);
+            print_output(output);
+
+            output = add_nuget_package_to_project(csproj_path.as_str(), "MediatR");
+            print_output(output);
+        }
+        project if project.contains(".Infrastructure") => {
+            output = add_project_reference(csproj_path.as_str(), &domain_csproj);
+            print_output(output);
+
+            output = add_nuget_package_to_project(csproj_path.as_str(), "AutoMapper");
+            print_output(output);
+        }
+        _ => {}
+    }
 }
