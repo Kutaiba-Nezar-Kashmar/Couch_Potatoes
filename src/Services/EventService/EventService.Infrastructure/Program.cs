430aab57f36a0dfc340cfcd3bd132ea4bd36cce2
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2023-05-12 12:59:23 +0200

    finish setting up compose and firestore (Michael, Kasper, Kutaiba)

--- src/Services/EventService/EventService.Infrastructure/Program.cs
+++ src/Services/EventService/EventService.Infrastructure/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Google.Api.Gax;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Firestore;
+
+var pathToServiceAccountKey = "serviceAccountKey.json";
+//Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", pathToServiceAccountKey);
+
+var credentials = GoogleCredential.FromFile(pathToServiceAccountKey);
+
+var db = new FirestoreDbBuilder
+{
+    ProjectId = "couch-potatoes-sep6",      
+    EmulatorDetection = EmulatorDetection.EmulatorOrProduction,
+    Credential = credentials
+}.Build();
