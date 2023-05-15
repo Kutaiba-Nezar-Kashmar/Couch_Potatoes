2f524dde8eab11eb63ce6ec008aac178877f384f
Michael Bui <michaelbui99@hotmail.com>
2023-05-15 12:57:02 +0200

    Make configuration resolve at runtime instead of compile time

--- src/Web/couch-potatoes-web-app/src/Services/movie-collection.ts
+++ src/Web/couch-potatoes-web-app/src/Services/movie-collection.ts
@@ -1,10 +1,11 @@
 import {useQuery} from "react-query";
-import config from "../couch-config.json";
 import {CacheKeys} from "./cache-keys"
+import {getConfig} from "../configuration/configuration";
 
 export function useFetchPopularMovies(skipPage: number, numberOfPages: number) {
     return useQuery({
         queryKey: [CacheKeys.POPULAR_MOVIES], queryFn: async () => {
+            const config = await getConfig();
             const response = await fetch(`${config.baseUrl}v1/movie-collection/popular?skip= ${skipPage}&numberOfPages=${numberOfPages}`)
             if (!response.ok)
                 throw new Error("Something went wrong");
---
+++ src/Web/couch-potatoes-web-app/src/configuration/configuration.ts
@@ -0,0 +1,11 @@
+export const CONFIG_PATH = process.env["PUBLIC_URL"];
+export const CONFIG_FILE = "couch-config.json";
+
+export interface CouchConfig {
+    baseUrl: string;
+}
+
+export async function getConfig(): Promise<CouchConfig> {
+    const configResponse = await fetch(`${CONFIG_PATH}/${CONFIG_FILE}`);
+    return await configResponse.json() as CouchConfig;
+}
