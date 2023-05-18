d3a6dabb94c8c59575261f1a0ed733f1231b968f
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2023-05-18 10:46:39 +0200

    removed todos

--- src/Web/couch-potatoes-web-app/src/pages/PersonDetailsPage.tsx
+++ src/Web/couch-potatoes-web-app/src/pages/PersonDetailsPage.tsx
@@ -12,11 +12,9 @@ const PersonDetailsPage = () => {
 
     useEffect(() => {
         if (!isLoading) {
-            // NOTE: (mibui 2023-05-15) Take the most popular movie as featured.
-            // TODO: (mibui 2023-05-15) Fetch from /movies/:movieid instead of taking from collection.
-            //                          e.g. setMovie(getMovie((collections as any)!["popular"]["collection"][0]?.id))
+
             setPerson(personData as PersonDetails);
-            console.log(personData)// TODO: Remove the genres, they are only there for testing purposes
+            console.log(personData)
         }
     }, [isLoading])
 
