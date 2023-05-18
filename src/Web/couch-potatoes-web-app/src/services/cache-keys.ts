28fa6053144a1dae8e4628d46e28c0cf6677ed80
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2023-05-18 10:23:12 +0200

    person details service

--- src/Web/couch-potatoes-web-app/src/services/cache-keys.ts
+++ src/Web/couch-potatoes-web-app/src/services/cache-keys.ts
@@ -1,4 +1,5 @@
 export enum CacheKeys {
     POPULAR_MOVIES = "popular_movies",
-    ALL_COLLECTIONS = "all_collections"
+    ALL_COLLECTIONS = "all_collections",
+    PERSON_DETAILS = "person_details"
 }
---
+++ src/Web/couch-potatoes-web-app/src/services/person-service/person-details-service.ts
@@ -0,0 +1,26 @@
+import {useQuery} from "react-query";
+import {CacheKeys} from "../cache-keys";
+import {getConfig} from "../../configuration/configuration";
+import PersonDetails from "../../models/person/person-details";
+
+export function useFetchPersonDetails(options: FetchPersonDetailsOptions) {
+    return useQuery({
+        queryKey: [CacheKeys.PERSON_DETAILS + options.personId], queryFn: async (): Promise<object> => {
+            return {
+                person: await fetchPersonDetails(options)
+            }
+        }
+    })
+}
+
+export interface FetchPersonDetailsOptions {
+    personId: number
+}
+
+// TODO: remember to fetch the base uri later from configs when we setup the gateway
+async function fetchPersonDetails(options: FetchPersonDetailsOptions): Promise<PersonDetails> {
+    const {personId} = options;
+    const response = await fetch(`https://localhost:8084/couch-potatoes/api/v1/person/details/${personId}`);
+    if (!response.ok) throw new Error("Cannot fetch person details");
+    return response.json();
+}
