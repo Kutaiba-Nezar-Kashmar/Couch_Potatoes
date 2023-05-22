93bca259a184696c1a8cb80910153f1a73f23bfe
Michael Bui <michaelbui99@hotmail.com>
2023-05-22 10:47:44 +0200

    Add hook for fetching multiple movies

--- src/Web/couch-potatoes-web-app/src/services/cache-keys.ts
+++ src/Web/couch-potatoes-web-app/src/services/cache-keys.ts
@@ -1,7 +1,8 @@
 export enum CacheKeys {
-    POPULAR_MOVIES = "popular_movies",
-    ALL_COLLECTIONS = "all_collections",
-    TOP_RATED = "top_rated",
-    CURRENT_MOVIE = "current_movie",
-    MOVIE_CREDITS ="movie_credits"
+    POPULAR_MOVIES = 'popular_movies',
+    ALL_COLLECTIONS = 'all_collections',
+    TOP_RATED = 'top_rated',
+    CURRENT_MOVIE = 'current_movie',
+    MOVIE_CREDITS = 'movie_credits',
+    MOVIES = 'movies_',
 }
