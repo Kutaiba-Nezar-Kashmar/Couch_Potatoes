4a5b9fb8b068f15f73e6abf407f63ada585ef847
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-05-23 12:37:18 +0200

    add pagination back to moviepage

--- src/Web/couch-potatoes-web-app/src/pages/FullCastAndCrewPage.tsx
+++ src/Web/couch-potatoes-web-app/src/pages/FullCastAndCrewPage.tsx
@@ -4,7 +4,7 @@ import {useNavigate, useParams} from "react-router-dom";
 import {MovieCreditsAndDetails, useFetchMovieCreditsAndMovies} from "../services/movie-credits-and-details";
 import {useFetchMovieCredits} from "../services/movie-credits";
 import {
-    Box, Card, CardBody,
+    Box, Button, Card, CardBody,
     Flex,
     Grid,
     GridItem, Heading, Image,
@@ -30,23 +30,27 @@ import {
 import {MovieDetailsReviewsComponent} from "../components/movie-details/MovieDetailsReviewsComponent";
 import backgroundImageFull from "../components/BackgroundImageFull";
 import {PersonCastCardComponent} from "../components/PersonCastCardComponent";
+import {useFetchMovieDetails} from "../services/movie-details";
+import Movie from "../models/movie";
 
 const FullCastAndCrewPage = () => {
+    const {movieId} = useParams()
     const Background_Temp = 'https://static1.cbrimages.com/wordpress/wp-content/uploads/2023/02/john-wick-4-paris-poster.jpg';
     const Empty_Profile_Pic = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png';
-    const {movieId} = useParams()
 
     const [movieCredits, setMovieCredits] = useState<MovieCredits | null>(null);
+    const [movie, setMovie] = useState<Movie | null>(null);
     const themeColor = "teal";
     const navigate = useNavigate();
-    const {isLoading, isError, data, error} = useFetchMovieCredits(Number(movieId));
+    const {isLoading, isError, data, error} = useFetchMovieCreditsAndMovies(Number(movieId));
 
 
     useEffect(() => {
         if (!isLoading) {
             setMovieCredits(
-                data ?? null
+                data?.credits ?? null
             );
+            setMovie(data?.movieDetails ?? null);
         }
     }, [isLoading])
 
@@ -77,7 +81,14 @@ const FullCastAndCrewPage = () => {
                 <Grid autoFlow={"dense"}
                       templateColumns={{base: "6fr", md: "repeat(6, 1fr)", lg: "repeat(6, 1fr)"}} gap={4}
                 >
-
+                    <GridItem justifySelf="center" alignSelf="center" colSpan={6}>
+                        <Heading textTransform='uppercase' color={"white"}>Cast and crew for {movie?.title} </Heading>
+                    </GridItem>
+                    <GridItem colSpan={6}>
+                        <Button onClick={() => navigate(`/movie/details/${movie?.id}`)} margin={0.5}
+                                colorScheme={themeColor} size='xs'
+                        >Return to movie page</Button>
+                    </GridItem>
                     <GridItem colSpan={3} gap={4}>
                         <Stack direction={"row"}>
                             <Heading color={"white"}>Cast </Heading>
