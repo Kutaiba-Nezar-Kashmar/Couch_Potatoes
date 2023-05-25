41b467d726696fe73e7fc03842c455bf411465c1
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-05-25 11:21:53 +0200

    implmented search for movies and redirect to movie page

--- src/Web/couch-potatoes-web-app/src/components/Search/SearchBar.tsx
+++ src/Web/couch-potatoes-web-app/src/components/Search/SearchBar.tsx
@@ -1,11 +1,39 @@
 import React, {FC, useEffect, useState} from "react";
-import {Box, Input, InputGroup, InputLeftElement, List, ListItem, VStack, Text} from "@chakra-ui/react";
+import {
+    Box,
+    Input,
+    InputGroup,
+    InputLeftElement,
+    List,
+    ListItem,
+    VStack,
+    Text,
+    Stack,
+    Image,
+    BoxProps, Flex, StackDivider
+} from "@chakra-ui/react";
 import {SearchIcon} from "@chakra-ui/icons";
 import {useFetchPersonDetailsAndCredits} from "../../services/person-service/person-details-service";
 import {multiSearch} from "../../services/search/search-service";
 import MultiSearchResponse from "../../models/search/multi-search-response";
+import {getPosterImageUri} from "../../services/images";
+import {useNavigate} from "react-router-dom";
+const scrollbarStyles = {
+    width: "8px",
+    backgroundColor: "white",
+    borderRadius: "4px",
+};
 
-export const SearchBar = () => {
+const hoverStyles = {
+    backgroundColor: "darkgray",
+};
+interface searchBarProperties {
+  width:number
+}
+export const SearchBar: FC<searchBarProperties> = ({width}) => {
+    const Background_Temp =
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png';
+
     function debounce(func: Function, delay: number) {
         let timerId: NodeJS.Timeout;
 
@@ -24,11 +52,13 @@ export const SearchBar = () => {
         const {value} = event.target;
         setSearchTerm(value);
     };
-    const debouncedSearchTerm = debounce(handleSearchInputChange, 3000);
+
+    const debouncedSearchTerm = debounce(handleSearchInputChange, 1000);
     const performSearch = async () => {
-    const result = await multiSearch(searchTerm);
+        const result = await multiSearch(searchTerm);
         setSearchResults(result ?? null);
     };
+    const navigate = useNavigate();
 
     // Execute the search logic when the debounced search term changes
     useEffect(() => {
@@ -38,22 +68,47 @@ export const SearchBar = () => {
     return (
         <>
             <VStack spacing={4} align="stretch">
-                <InputGroup width={{base: '300px', md: '450px', lg: '600px'}}>
+                <InputGroup width={[width/2.5,width]}>
                     <InputLeftElement
                         pointerEvents="none"
                         children={<SearchIcon color="gray.300"/>}
                     />
-                    <Input value={searchTerm} onChange ={handleSearchInputChange} bg="white" type="text"
+                    <Input onChange={debouncedSearchTerm} bg="white" type="text"
                            placeholder="Search"/>
                 </InputGroup>
                 <Box>
-                    <List spacing={2}>
-                        {searchResults?.movies?.map((result) => (
-                            <ListItem key={result.id}>
-                                <Text color='white'>{result.title}</Text>
-                            </ListItem>
-                        ))}
-                    </List>
+                    {searchTerm ? (
+                        <List  spacing={"1px"}  boxShadow="md" position={"absolute"}  bg={"grey"} width={[width/2,width]} height="300px"
+                               overflow="auto" zIndex="9999"  sx={{
+                            "&::-webkit-scrollbar": scrollbarStyles,
+                            "&::-webkit-scrollbar-thumb": hoverStyles,
+                            "&::-webkit-scrollbar-thumb:hover": hoverStyles,
+                        }} >
+                            {searchResults?.movies?.map((result) => (
+                                <ListItem padding={"2px"} bg={"white"} key={result.id} onClick={() =>
+                                    navigate(`/Couch_Potatoes/movie/details/${result.id}`)}>
+                                    <Stack
+                                        direction={"row"} overflowX={"auto"} _hover={{cursor: "pointer"}}>
+                                        {(result?.posterPath) ? (<Image maxHeight={"80px"}
+                                                                        src={getPosterImageUri(result?.posterPath)}
+                                                                        alt={"poster of movie" + result.title}
+                                                                        borderRadius='lg'
+                                        />) : (<Image maxHeight={"80px"}
+                                                      maxWidth={"53px"}
+                                                      src={Background_Temp}
+                                                      alt={"no poster of movie" + result.title}
+                                                      borderRadius='lg'
+                                        />)}
+                                        <Flex alignItems={"center"}>
+                                            <Text>{result.title}</Text>
+                                        </Flex>
+
+                                    </Stack>
+
+                                </ListItem>
+                            ))}
+                        </List>) : null}
+
                 </Box>
             </VStack>
         </>
