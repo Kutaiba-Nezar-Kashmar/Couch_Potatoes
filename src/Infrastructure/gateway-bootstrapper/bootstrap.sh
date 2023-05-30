c1aac6850714b1305051f8491e0d75b6bcf1eaf3
Michael Bui <michaelbui99@hotmail.com>
2023-05-30 12:37:10 +0200

    Fix gateway bootstrapper image

--- src/Infrastructure/gateway-bootstrapper/bootstrap.sh
+++ src/Infrastructure/gateway-bootstrapper/bootstrap.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+echo Bootstrapping gateway...
 
 export KONG_DATABASE=off
 export KONG_DECLARATIVE_CONFIG=/bootstrapper/kong/declarative/kong.yaml
@@ -10,4 +12,4 @@ export KONG_ADMIN_LISTEN=0.0.0.0:8001
 echo Kong API Gateway starting with configuration:
 cat /bootstrapper/kong/declarative/kong.yaml
 
-./bootstrapper/gateway-bootstrapper && /docker-entrypoint.sh kong docker-start
+/bootstrapper/gateway-bootstrapper && /docker-entrypoint.sh kong docker-start
