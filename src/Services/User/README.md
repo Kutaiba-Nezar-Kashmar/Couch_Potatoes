0b6e7d46d3b0c0b50da9cbb8e6245ab08338691d
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2023-05-30 10:38:39 +0200

    Read me for user servioce

---
+++ src/Services/User/README.md
@@ -0,0 +1,10 @@
+﻿# User Service
+User service is responsible for:</br>
+- Handling creating couch potatoes accounts for users.
+- Handling login for users.
+- Keeping a list of favorite movies for a user.
+- Handling the creation of reviews for a movie by a user.
+- Handling of adding a vote for a movie review bu the user.
+- Removing a movie review created by the user.
+- Removing a vote for a movie review by the user.
+- Retrieve relevant details for a user.
--- src/Services/User/User.Domain/Exceptions/FailedToCreateReviewException.cs
+++ src/Services/User/User.Domain/Exceptions/FailedToCreateReviewException.cs
@@ -1,9 +1,10 @@
 namespace User.Domain.Exceptions;
 
-public class FailedToCreateReviewException: Exception
+public class FailedToCreateReviewException : Exception
 {
-    public FailedToCreateReviewException(int movieId,string? reason,  Exception? inner = null):base($"Failed to create review for movie {movieId}: {reason?? ""}", inner)
+    public FailedToCreateReviewException(int movieId, string? reason,
+        Exception? inner = null) : base(
+        $"Failed to create review for movie {movieId}: {reason ?? ""}", inner)
     {
-        
     }
 }
